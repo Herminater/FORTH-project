977e61cabe561c34272c500b641512ed605c6bb2
Herman Skovbjerg <AU805182@uni.au.dk>
2025-09-30 20:40:27 +0200

    HASHMAP VIRKER

--- hashmap_implementation.c
+++ hashmap_implementation.c
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define HASH_LENGTH 10
-#define KEY_MAX_LENGTH 5
+#define HASH_LENGTH 100
+#define KEY_MAX_LENGTH 50
+
+/*
+Functions:
+    generate_hash(char key) -> int
+    init_bucket(int hsh, char k[], int value) -> bucket * 
+    init_hashMap() -> hashMap
+    put(hashMap *map, char key[], int val) -> void
+    get(hashMap *map, char key[]) -> int
+
+Structures:
+    bucket(hash, key, value)
+    hashMap()
+*/
 
 typedef struct bucket{
     int hash;
@@ -13,9 +26,10 @@ typedef struct bucket{
 } bucket;
 
 typedef struct hashMap{
-    struct bucket * b[HASH_LENGTH];
+    bucket * b[HASH_LENGTH];
 } hashMap;
 
+// generates hashvalue and assigns index within max_length of the hash map
 int generate_hash(char key[]){
     int first = (int)key[0]*100;
     int second = (int)key[1]*1000;
@@ -23,6 +37,7 @@ int generate_hash(char key[]){
     return hash;
 }
 
+// creates a new bucket for the hashmap
 bucket * init_bucket(int hsh, char k[], int value){ 
     bucket *bu = (bucket *) malloc(sizeof(bucket));
     bu->hash = hsh;
@@ -32,6 +47,7 @@ bucket * init_bucket(int hsh, char k[], int value){
     return bu;
 }
 
+// initializes an empty hashmap with size hash_length
 hashMap init_hashMap(){
     struct hashMap map;
     for (int i=0; i<HASH_LENGTH; i++){
@@ -41,51 +57,58 @@ hashMap init_hashMap(){
 
 }
 
+// inserts key,value pair into hashmap
 void put(hashMap *map, char key[], int val){
     int hash = generate_hash(key);
-    bucket new_b = *init_bucket(hash, key, val);
 
     // check for colisions
+    // if no collisions:
     if (map->b[hash] == NULL){
-        printf("H");
-        map->b[hash] = &new_b;
+        map->b[hash] = init_bucket(hash, key, val);
+        
     }
+    // if colission
     else{
         struct bucket * curr = map->b[hash];
-
-        do{
-            if (strcmp(key, curr->key) == 0){
-                map->b[hash] = &new_b;
+        do{ 
+            // if value allready present:
+            if (strcmp(key, curr->key) == 0){ 
+                curr->value = val;
+                return;
+            }
+            // if hash is same but value is different
+            if (curr->next == NULL){
+                curr->next = init_bucket(hash,key,val);
                 return;
             }
             curr = curr->next;
 
-        }while(curr->next != NULL);
-
-        curr->next = &new_b;
+        }while(1);
     }
 }
 
+// fetches value with key
 int get(hashMap *map, char key[]){
     int hash = generate_hash(key);
-
     struct bucket * curr = map->b[hash];
 
-    if (curr == NULL){
+    if (curr == NULL){ // if nothing on index
         return -11111;
     }
+    // if something on index
+    do{ 
+        // if match
+        if (strcmp(key, curr->key) == 0){
+            return curr->value;
+        }
+        curr = curr->next;
 
-    do{
-            if (strcmp(key, curr->key) == 0){
-                return curr->value;
-            }
-            curr = curr->next;
+    }
+    while(curr != NULL);
 
-        }while(curr != NULL);
-
+    // if something on index but no match
     return -11111;   
 }
-
 
 int main(){
     hashMap map = init_hashMap();
@@ -95,15 +118,15 @@ int main(){
     put(&map, "test", 1);
     put(&map, "temp", 2);
     put(&map, "telt", 3);
+    put(&map, "test", 3);
+
 
     int val = get(&map, "test");
     int val2 = get(&map, "temp");
     int val3 = get(&map, "telt");
 
-    printf("test er %d", val);
-    printf("temp er %d", val2);
-    printf("telt er %d", val3);
-
-    
+    printf("test er %d\n", val);
+    printf("temp er %d\n", val2);
+    printf("telt er %d\n", val3);
 
 }
