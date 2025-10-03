a5130917a677ccbc2103a10f0f1f92538f195b7b
Herman Skovbjerg <AU805182@uni.au.dk>
2025-10-03 18:20:10 +0200

    diverse funktioner implementeret

--- arrayStackplusHash.c
+++ arrayStackplusHash.c
@@ -54,7 +54,7 @@ void sub(){
     if (stackEnd > 0){
         int a = *pop();
         int b = *pop();
-        push(a - b);
+        push(b - a);
     };
 }
 
@@ -73,6 +73,117 @@ void mod(){
         push(b % a);
     };
 }
+
+void equals(){ // =
+    if (stackEnd > 0){
+        int a = *pop();
+        int b = *pop();
+        push(b == a);
+    };
+}
+
+void less(){ // >
+    if (stackEnd > 0){
+        int a = *pop();
+        int b = *pop();
+        push(a < b);
+    };
+}
+
+void more(){ // <
+    if (stackEnd > 0){
+        int a = *pop();
+        int b = *pop();
+        push(b < a);
+    };
+}
+
+void AND(){
+    if (stackEnd > 0){
+        int a = *pop();
+        int b = *pop();
+        push(b && a);
+    };
+}
+
+void OR(){
+    if (stackEnd > 0){
+        int a = *pop();
+        int b = *pop();
+        push(b && a);
+    };
+}
+
+void INVERT(){
+    if (stackEnd > 0){
+        int a = *pop();
+        push(!a);
+    };
+}
+
+void print(){ // .
+    if (stackEnd > 0){
+        int a = *pop();
+        printf("%d", a);
+    }
+}
+
+void EMIT(){ // int as ascii
+    if (stackEnd > 0){
+        int a = *pop();
+        printf("%c", (char)a);
+    }
+}
+
+void CR(){
+    printf("\n");
+}
+
+void dup(){
+    if (stackEnd > 0){
+        int a = *pop();
+        push(a);
+        push(a);
+    }
+}
+
+void drop(){
+    if (stackEnd > 0){
+        pop();
+    }
+}
+
+void swap(){
+    if (stackEnd > 0){
+        int a = *pop();
+        int b = *pop();
+        push(a);
+        push(b);
+    }
+}
+
+void over(){
+    if (stackEnd > 0){
+        int a = *pop();
+        int b = *pop();
+        push(a);
+        push(b);
+        push(a);
+    }
+}
+
+void rotate(){
+    if (stackEnd > 1){
+        int a = *pop();
+        int b = *pop();
+        int c = *pop();
+        push(b);
+        push(c);
+        push(a);
+    }
+}
+
+
 
 // deler strengen op i dele og pusher digits og kører funktioner
 void passString(char c[]){
@@ -102,6 +213,7 @@ void passString(char c[]){
         
     }
     printStack();
+    printf("\n");
 
 
 }
@@ -114,8 +226,20 @@ int main(void){
     put(&map, "+", &add);
     put(&map, "-", &sub);
     put(&map, "%", &mod);
-
-
+    put(&map, "=", &equals);
+    put(&map, ">", &less);
+    put(&map, "<", &more);
+    put(&map, "AND", &AND);
+    put(&map, "OR", &OR);
+    put(&map, "INVERT", &INVERT);
+    put(&map, ".", &print);
+    put(&map, "EMIT", &EMIT);
+    put(&map, "CR", &CR);
+    put(&map, "DUP", &dup);
+    put(&map, "DROP", &drop);
+    put(&map, "SWAP", &swap);
+    put(&map, "OVER", &over);
+    put(&map, "ROTATE", &rotate);
 
     bool flag = true;
     char c[100];
